5052f40e178e156f1a685ef7cc9d147a6857ec71
AzaZLO <MaloyMeee@yandex.ru>
2024-02-01 00:19:32 +0300

    print when bot start

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ dp.include_router(router)
 async def main() -> None:
     await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
     print("Бот запущен")
+    await dp.start_polling(bot)
 
 
 asyncio.run(main())
